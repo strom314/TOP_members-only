543a1c374dd885cdfd2593c8a54b5c83841c3de7
strom314 <jakub.gros777@gmail.com>
2026-01-16 19:08:08 +0100

    add a query to find user by username

--- db/query.js
+++ db/query.js
@@ -7,4 +7,11 @@ async function addUser(firstName, lastName, username, password, isAdmin) {
   );
 }
 
-module.exports = { addUser };
+async function findUserByUsername(username) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  return rows[0];
+}
+
+module.exports = { addUser, findUserByUsername };
