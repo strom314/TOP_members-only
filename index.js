079db79f5babd6ac98aef36ea47305f394106dff
strom314 <jakub.gros777@gmail.com>
2026-01-18 12:16:09 +0100

    add a get and post route for login implement passport with the login form

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const app = express();
 const path = require("node:path");
 const populateDb = require("./db/populateDb");
 const router = require("./routes/router");
+const passportStrategy = require("./config/passportConfig");
+const passport = require("passport");
+const sessionConfig = require("./config/sessionConfig");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
 
-populateDb();
+passport.use(passportStrategy);
+app.use(sessionConfig);
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// populateDb();
 
 app.use("/", router);
 
--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 const validator = require("../middleware/validator");
+const passport = require("passport");
 
 router.get("/", controller.getIndex);
+
 router.get("/sign-up", controller.getSignUp);
 router.post("/sign-up", validator.validateSignUp, controller.postSignUp);
+
+router.get("/log-in", controller.getLogIn);
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  })
+);
 
 module.exports = router;
